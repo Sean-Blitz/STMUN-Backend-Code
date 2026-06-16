3a4989cfb2b91f57a1bb26feecc8f9d09dcc2257
Sean-Blitz <siyanmu127@gmail.com>
2026-06-16 13:30:08 -0700

    Delete old Infrastructure folder inside SG Automations

--- SG Automations/Infrastructure/__init__.py
+++
@@ -1,7 +0,0 @@
-from .GoogleAPIs.DriveAPIManager import DriveAPI
-from .GoogleAPIs.GoogleAPIsManager import GoogleAPIs
-from .GoogleAPIs.SheetsAPIManager import SheetAPI
-
-from .Questionary.QuestionaryManager import QuestionaryClass
-
-from .CSV.CSVManager import CSV
